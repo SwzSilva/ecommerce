4ec7b4d73a554f4c7f3c147378307b9014a56dda
Schwarz <schwarzsilva96@gmail.com>
2019-02-13 11:34:09 -0200

    Tela de Login

---
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,33 @@
+<?php
+namespace Hcode;
+class Model{
+
+	private $values=[];
+
+	public function __call($name, $args){
+
+		$method = substr($name, 0,3);
+		$fieldName = substr($name, 3,strlen($name));
+
+		switch ($method) {
+			case 'get':
+				return $this->values[$fieldName];
+			break;
+			
+			case 'set':
+				$this->values[$fieldName] = $args[0];
+			break;
+		}
+	}
+	//Cria metodos dinamicamente
+	public function setData($data = array()){
+			foreach ($data as $key => $value) {
+				$this->{"set".$key}($value); // Criando metodos dinamicamente
+			}
+	}
+
+	public function getValues(){
+		return $this->values;
+	}
+}
+?>
